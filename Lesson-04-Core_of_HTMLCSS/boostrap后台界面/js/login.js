a53c198346429cdb999370bd9ca9c1c666c87e09
wangzheng <w_zhengcj@sina.com>
2016-11-21 15:30:47 +0800

    boostrap后台界面

---
+++ Lesson-04-Core_of_HTMLCSS/boostrap后台界面/js/login.js
@@ -0,0 +1,10 @@
+/**
+ * Created by WZJ on 2016/7/27.
+ */
+function checkName() {
+    var userLength = document.getElementById('username').value;
+    console.log(userLength);
+    //if(userLength.length<6){
+    //    alert("请输入至少6位用户名");
+    //}
+}
---
+++ Lesson-04-Core_of_HTMLCSS/boostrap后台界面/js/system.js
@@ -0,0 +1,6 @@
+/**
+ * Created by WZJ on 2016/7/26.
+ */
+$(".navbar-nav a").click(function(e){
+    $(this).tab("show");
+})
